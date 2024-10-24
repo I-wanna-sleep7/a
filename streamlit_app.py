a8cb2b2d72fcc2d085c9a0c2f6d73f5e7825c0e5
I-wanna-sleep7 <skyblue37mii@gmail.com>
2024-10-24 05:09:19 +0000

    main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,22 @@
 # Streamlitライブラリをインポート
 import streamlit as st
+import random
+for i in range(1,100):
+    x=random.randint(1,10000)
+    print(str(i)+"回目："+str(x))
+
+#おやつの決定
+if x%3==0:
+    print("今日のおやつはポッキー！"):
+elif x%3==1:
+    print("今日のおやつはグミ！"):
+else print("今日のおやつはクッキー！"):
 
 # ページ設定（タブに表示されるタイトル、表示幅）
 st.set_page_config(page_title="タイトル", layout="wide")
 
 # タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+st.title('今日のおやつ')
 
 # テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
 user_input = st.text_input('あなたの名前を入力してください')
@@ -13,19 +24,17 @@ user_input = st.text_input('あなたの名前を入力してください')
 # ボタンを作成し、クリックされたらメッセージを表示
 if st.button('挨拶する'):
     if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
+        st.success(f'🌟 やっほー、{user_input}さん! 🌟')  # メッセージをハイライト
     else:
         st.error('名前を入力してください。')  # エラーメッセージを表示
 
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
+#数字の入力
+write_number= st.text_input("10進数の数字を入力してください")
+if st.button("クリック"):
+    st.write("ボタンがクリックされました!")
+
 
 # 補足メッセージ
 st.caption("十字キー（左右）でも調整できます。")
 
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+#　奇数と偶数の仕分け
